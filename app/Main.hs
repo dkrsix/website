54f5e1365d2eb9a04a65728b862adb20eb28d561
dkr6 <dylank@posteo.com>
2025-09-14 17:55:01 -0500

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,6 @@
 module Main where
 
 import CMark
-import Data.Text as T
 import Data.Text.IO as TIO
 import System.Environment
 import System.Exit
@@ -12,12 +11,12 @@ import Text.Blaze.Html5.Attributes as A
 import Text.Blaze.Html.Renderer.String
 
 main :: IO ()
-main = getArgs >>= parse >>= Prelude.writeFile "html/index.html" . renderHtml . Main.template 
+main = getArgs >>= parse >>= Prelude.writeFile "html/index.html" . renderHtml . Main.template
 
-parse :: [String]-> IO Text 
+parse :: [String]-> IO Text
 parse ["-h"]= usage >> exit
 parse [fp] = do
-  text <- TIO.readFile fp 
+  text <- TIO.readFile fp
   return $ commonmarkToHtml [] text
 
 template :: Text -> Html
@@ -32,4 +31,3 @@ usage = Prelude.putStrLn "Usage: website [-vh [file...]"
 
 exit :: IO Text
 exit = exitWith ExitSuccess
-

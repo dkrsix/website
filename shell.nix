22b28d6ac68b9fbe3c5dda4b91d314355deedce5
dkrsix <dylank@posteo.com>
2025-09-22 08:00:44 -0500

    make it so it doesnt use npins

--- shell.nix
+++ shell.nix
@@ -1,6 +1,9 @@
 let
-  sources = import ./npins;
-  pkgs = import sources.nixpkgs { overlays = [ (import sources.rust-overlay) ]; };
+
+  pkgs = import <nixpkgs> { overlays = [ (import
+        (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))
+    ];
+  };
 in
 pkgs.mkShell {
   buildInputs = with pkgs; [
